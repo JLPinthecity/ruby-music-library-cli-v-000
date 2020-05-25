9a3319b8fbab48f6d8740f16ad04534ff792b41c
Joann Pan <joannlpan@gmail.com>
2020-05-25 17:10:04 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -73,6 +73,18 @@ class MusicLibraryController
     end
   end
 
+  def list_songs_by_genre
+    puts "Please enter the name of a genre:"
+    input = gets.strip 
+    if genre = Genre.find_by_name(input)
+      genre.songs.sort do |a, b|
+        a.name <=> b.name
+      end.each.with_index(1) do |song, index|
+        puts "#{index}. #{song.artist.name} - #{song.name}"
+      end
+    end
+  end
+
 
 
 
