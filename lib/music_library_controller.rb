099d9025386a5a5dcd92aa7799be5f23c04a488f
Joann Pan <joannlpan@gmail.com>
2020-05-25 15:42:39 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -38,11 +38,11 @@ class MusicLibraryController
   end
 
   def list_songs
-    files = MusicImporter.files 
+    files = MusicImporter.files
     files.each.with_index(1) do |file|
       binding.pry
     end
-    
+
   end
 
 
