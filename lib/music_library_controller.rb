16b564149203aa5fecd5d50e5c1b08704147e22e
Joann Pan <joannlpan@gmail.com>
2020-05-25 17:17:37 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -90,7 +90,7 @@ class MusicLibraryController
     input = gets.strip
     index = input.to_i
     songs = list_songs
-    song = songs[index]
+    song = songs[index - 1]
     puts "Playing #{song.name} by #{song.artist}"
   end
 
